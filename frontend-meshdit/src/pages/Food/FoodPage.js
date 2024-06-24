44f36561f882fde2d8b76d4fd229e9025b7daeaf
Whizzion957 <aadit_ks@cs.iitr.ac.in>
2024-06-24 22:14:04 +0530

    Phase 5 done

--- frontend-meshdit/src/pages/Food/FoodPage.js
+++ frontend-meshdit/src/pages/Food/FoodPage.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import classes from './foodPage.module.css';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getByID } from '../../services/foodService';
 import StarRating from '../../components/StarRating/StarRating';
 import Tags from '../../components/Tags/Tags';
 import Price from '../../components/Price/Price';
+import { useCart } from '../../hooks/useCart';
 
 export default function FoodPage() {
     const [food, setFood] = useState({});
     const {id} = useParams();
-
+    const {addToCart} = useCart();
+    const navigate = useNavigate();
+    const handleAddToCart = () => {
+        addToCart(food);
+        navigate('/cart');
+    }
     useEffect(() => {
         getByID(id).then(setFood);
     }, [id]);
@@ -49,7 +55,7 @@ export default function FoodPage() {
                     <Price price={food.price} />
                 </div>
 
-                <button>Add To Cart</button>
+                <button onClick={handleAddToCart}>Add To Cart</button>
             </div>
         </div>}
     </>
