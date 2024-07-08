e2c4b75ea4511ab752ff71196f51042a9bcf802f
Whizzion957 <aadit_ks@cs.iitr.ac.in>
2024-07-08 12:43:47 +0530

    Phase 18 done

--- backend-meshdit/src/server.js
+++ backend-meshdit/src/server.js
@@ -34,7 +34,7 @@ app.get('*', (req,res) => {
     res.send(indexFilePath);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('listening on port' + PORT);
 });
